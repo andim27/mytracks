7fe9f84e5aec17ddacae6da8882ed8c2b45a4567
amk <a2772@mail.ru>
2016-10-31 11:22:31 +0200

    set repo link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { MdToolbar, MdInput } from "@angular/material"
 import { Tracks } from './tracks';
 import { TracksService } from './tracks.service';
 
@@ -8,7 +7,6 @@ import { TracksService } from './tracks.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [TracksService]
-  // directives: ['MdToolbar']
 })
 export class AppComponent  implements OnInit {
   title = 'My music tracks ';
@@ -20,16 +18,16 @@ export class AppComponent  implements OnInit {
   }
   private initMusicsList() {
     this.musicsList = [];
-    console.log('InitMusicsList...');
+    // console.log('InitMusicsList...');
   };
   ngOnInit() {
     this._tracksservice.getInitialMusicTracks().then(musics => this.musicsList = musics);
   };
   goMyProfile() {
-    window.location.href = 'https://github.com/andim27';
+    window.location.href = 'https://github.com/andim27/mytracks';
   };
   search() {
-    console.log('!!! search_name=' + this.search_name);
+    // console.log('!!! search_name=' + this.search_name);
     this._tracksservice.getMusicTracks(this.search_name).subscribe (
                         data => this.getItemsFromServer(data),
                         () => console.log('Api Done Get !')
@@ -37,7 +35,7 @@ export class AppComponent  implements OnInit {
   };
   getItemsFromServer(data) {
     let items = data.tracks.items;
-    console.log('getItemFromServer items.length=' + items.length);
+    // console.log('getItemFromServer items.length=' + items.length);
     this.initMusicsList();
     for (let item of items) {
       this.musicsList.push({track_name: item.name, album_name: item.album.name, author_name: item.album.artists[0].name});
